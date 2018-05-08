94d14239694a0739d9e5264b08cf589223ec7cd7
Sekazedy <sekazedy@gmail.com>
2018-05-08 22:23:05 +0300

    #4 - authentication and route protection

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <style>
+        .input-group label {
+            text-align: left;
+        }
+    </style>
+
+    @if(count($errors) > 0)
+        <section class="info-box fail">
+            @foreach($errors->all() as $error)
+                {{ $error }}
+            @endforeach
+        </section>
+    @endif
+
+    @if(Session::has('fail'))
+        <section class="info-box fail">
+            {{ Session::get('fail') }}
+        </section>
+    @endif
+
+    <form action="{{ route('admin.login') }}" method="POST">
+        <div class="input-group">
+            <label for="name">Your Name</label>
+            <input type="text" name="name" id="name" placeholder="Your Name">
+        </div>
+        <div class="input-group">
+            <label for="password">Your Password</label>
+            <input type="password" name="password" id="password" placeholder="Your Password">
+        </div>
+        <button type="submit">Log In</button>
+        <input type="hidden" name="_token" value="{{ Session::token() }}">
+    </form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,30 @@ Route::group(['middleware' => ['web']], function () {
         'uses'  => 'QuoteController@getMailCallback',
         'as'    => 'mail_callback'
     ]);
+
+    Route::get('/admin/login', [
+        'uses'  => 'AdminController@getLogin',
+        'as'    => 'admin.login'
+    ]);
+
+    Route::post('/admin/login', [
+        'uses'  => 'AdminController@postLogin',
+        'as'    => 'admin.login'
+    ]);
+
+    Route::group(['middleware' => 'auth'], function() {
+        Route::get('/admin/dashboard', [
+            'uses'  => 'AdminController@getDashboard',
+            'as'    => 'admin.dashboard'
+        ]);
+
+        Route::get('/admin/quotes', function(){
+            return view('admin.quotes');
+        });
+    });
+
+    Route::get('/admin/logout', [
+        'uses'  => 'AdminController@getLogout',
+        'as'    => 'admin.logout'
+    ]);
 });
